a08101b46febfb7e7957acd2795f7a87e517d703
Abdul Wahab <143833600+abdulwahab458@users.noreply.github.com>
2026-05-23 22:42:44 +0530

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+   public:
+    void reorderList(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* prev = NULL;
+        ListNode* curr = slow;
+
+        while (curr != NULL) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        ListNode* temp = head;
+        ListNode* ans = head;
+        while (prev->next != NULL){
+            ListNode* a = temp->next;
+            ListNode* b = prev->next;
+            temp->next = prev;
+            prev->next = a;
+            temp = a; 
+            prev = b ;
+        }
+
+
+    }
+};
